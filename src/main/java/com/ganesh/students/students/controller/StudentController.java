db393440631f9d7902a09dd56ff70851435179d5
GaneshShiv <gnsh.shiv@gmail.com>
2020-03-02 21:21:00 +0530

    Initial version

---
+++ src/main/java/com/ganesh/students/students/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.ganesh.students.students.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.students.students.service.StudentService;
+import com.ganesh.students.students.model.Course;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	@GetMapping("/students")
+	public String retrieveCoursesForStudent() {
+		return "Hi";
+	}
+	@GetMapping("/students/{studentId}/courses")
+	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+		return studentService.retrieveCourses(studentId);
+	}
+	
+	
+
+}
